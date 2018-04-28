6bc974b9e5137a1ec570613fff0ed56db635226d
daisymae <daisymae@orcadiafarm.com>
2018-04-28 12:46:37 -0500

    Enhance directive
    Enhance custom directive to allow for inputs to provide more
    customization capabilities.

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,18 +1,27 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  // custom property binding
+  @Input() defaultBackgroundColor = 'transparent';
+  @Input() highlightColor = 'blue';
+  @Input() defaultColor = 'black';
+  @Input() hoverColor = 'white';
+  @Input() afterVisitColor = 'purple';
+
   // input is the property we want to bind to
   // alternative to using Renderer2
   // can bind to any property of element sitting on
-  @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
-  @HostBinding('style.color') color = 'black';
+  @HostBinding('style.backgroundColor') backgroundColor;
+  @HostBinding('style.color') color;
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+    this.backgroundColor = this.defaultBackgroundColor;
+    this.color = this.defaultColor;
     // better to use the renderer; need to access the nativeElement
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
     // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
@@ -21,15 +30,15 @@ export class BetterHighlightDirective implements OnInit {
   @HostListener('mouseenter') mouseover(eventData: Event) {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
     // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
-    this.backgroundColor = 'blue';
-    this.color = 'white';
+    this.backgroundColor = this.highlightColor;
+    this.color = this.hoverColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
     // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultBackgroundColor;
     // simulate a link visited by changing the color after a hover
-    this.color = 'purple';
+    this.color = this.afterVisitColor;
   }
 }
