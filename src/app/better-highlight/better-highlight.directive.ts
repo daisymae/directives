4cdb99d8bb67778161ed7b776059677ca74f529f
daisymae <daisymae@orcadiafarm.com>
2018-04-28 12:32:55 -0500

    Alternative
    Alternative way to handle modifying a property by using @HostBinding
    instead of Renderer2. Nothing wrong with Renderer2, just another way of
    doing it.

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,9 +1,15 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  // input is the property we want to bind to
+  // alternative to using Renderer2
+  // can bind to any property of element sitting on
+  @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
+  @HostBinding('style.color') color = 'black';
+
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
@@ -13,12 +19,17 @@ export class BetterHighlightDirective implements OnInit {
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+    this.backgroundColor = 'blue';
+    this.color = 'white';
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
+    this.backgroundColor = 'transparent';
+    // simulate a link visited by changing the color after a hover
+    this.color = 'purple';
   }
 }
